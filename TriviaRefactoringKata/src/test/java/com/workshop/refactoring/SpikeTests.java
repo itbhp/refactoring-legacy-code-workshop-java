307998ce2f34d78850bb80f4e50d9c67748caeef
Paolo Sciarra <paolo.sciarra@lastminute.com>
2017-10-24 11:55:37 +0200

    Tried to register output

--- TriviaRefactoringKata/src/test/java/com/workshop/refactoring/SpikeTests.java
+++ TriviaRefactoringKata/src/test/java/com/workshop/refactoring/SpikeTests.java
@@ -2,9 +2,15 @@ package com.workshop.refactoring;
 
 import org.junit.Test;
 
+import java.io.PrintStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class SpikeTests {
   @Test
   public void name() throws Exception {
+    Path path = Paths.get("game-output3.txt");
+    System.setOut(new PrintStream(path.toFile()));
     GameRunner.main(new String[0]);
   }
 }
