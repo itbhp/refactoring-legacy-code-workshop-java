be865c72658078d245017b3fe7e34a01b5b47fd6
Paolo Sciarra <paolo.sciarra@lastminute.com>
2017-10-24 12:28:24 +0200

    added assert

--- TriviaRefactoringKata/src/test/java/com/workshop/refactoring/SpikeTests.java
+++ TriviaRefactoringKata/src/test/java/com/workshop/refactoring/SpikeTests.java
@@ -3,8 +3,12 @@ package com.workshop.refactoring;
 import org.junit.Test;
 
 import java.io.PrintStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class SpikeTests {
   @Test
@@ -12,5 +16,9 @@ public class SpikeTests {
     Path path = Paths.get("game-output.txt");
     System.setOut(new PrintStream(path.toFile()));
     GameRunner.main(new String[0]);
+    String expected = String.valueOf(Files.readAllLines(Paths.get("expected-game-output.txt")));
+    String actual = String.valueOf(Files.readAllLines(Paths.get("game-output.txt")));
+
+    assertThat(expected, is(actual));
   }
 }
