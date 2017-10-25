5fc390cc6766a7e86a4b1cabf5cd922024420349
Paolo Sciarra <itbhps@gmail.com>
2017-10-25 11:14:24 +0200

    removed some duplication

--- TriviaRefactoringKata/src/main/java/com/workshop/refactoring/QuestionDeck.java
+++ TriviaRefactoringKata/src/main/java/com/workshop/refactoring/QuestionDeck.java
@@ -1,6 +1,8 @@
 package com.workshop.refactoring;
 
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 
 public class QuestionDeck {
@@ -8,54 +10,59 @@ public class QuestionDeck {
   private LinkedList scienceQuestions;
   private LinkedList sportsQuestions;
   private LinkedList rockQuestions;
+  private final List<Integer> popPlaces;
+  private final List<Integer> sciencePlaces;
+  private final List<Integer> sportsPlaces;
+  private final List<Integer> rockPlaces;
 
   public QuestionDeck() {
     popQuestions = new LinkedList();
     scienceQuestions = new LinkedList();
     sportsQuestions = new LinkedList();
     rockQuestions = new LinkedList();
+    popPlaces = Arrays.asList(0, 4, 8);
+    sciencePlaces = Arrays.asList(1, 5, 9);
+    sportsPlaces = Arrays.asList(2, 6, 10);
+    rockPlaces = Arrays.asList(3, 7, 11);
   }
 
   void fillQuestions() {
-      for (int i = 0; i < 50; i++) {
-          popQuestions.addLast("Pop Question " + i);
-          scienceQuestions.addLast(("Science Question " + i));
-          sportsQuestions.addLast(("Sports Question " + i));
-          rockQuestions.addLast("Rock Question " + i);
-      }
+    for (int i = 0; i < 50; i++) {
+      popQuestions.addLast(createIndexedQuestion(i, "Pop"));
+      scienceQuestions.addLast(createIndexedQuestion(i, "Science"));
+      sportsQuestions.addLast(createIndexedQuestion(i, "Sports"));
+      rockQuestions.addLast(createIndexedQuestion(i, "Rock"));
+    }
+  }
+
+  private String createIndexedQuestion(int i, final String category) {
+    return category + " Question " + i;
   }
 
   String currentCategoryForPlaces(int place) {
-      if (place == 0) return "Pop";
-      if (place == 4) return "Pop";
-      if (place == 8) return "Pop";
-      if (place == 1) return "Science";
-      if (place == 5) return "Science";
-      if (place == 9) return "Science";
-      if (place == 2) return "Sports";
-      if (place == 6) return "Sports";
-      if (place == 10) return "Sports";
-      return "Rock";
+    if (popPlaces.contains(place)) return "Pop";
+    if (sciencePlaces.contains(place)) return "Science";
+    if (sportsPlaces.contains(place)) return "Sports";
+    if (rockPlaces.contains(place)) return "Rock";
+
+    return "Rock";
   }
 
   Object askQuestionForCategory(String category) {
-      Object question = null;
-      if (Objects.equals(category, "Pop")) {
-        question = popQuestions.removeFirst();
-        System.out.println(question);
-      }
-      else if (Objects.equals(category, "Science")) {
-        question = scienceQuestions.removeFirst();
-        System.out.println(question);
-      }
-      else if (Objects.equals(category, "Sports")) {
-        question = sportsQuestions.removeFirst();
-        System.out.println(question);
-      }
-      else if (Objects.equals(category, "Rock")) {
-        question = rockQuestions.removeFirst();
-        System.out.println(question);
-      }
-      return question;
+    Object question = null;
+    if (Objects.equals(category, "Pop")) {
+      question = popQuestions.removeFirst();
+      System.out.println(question);
+    } else if (Objects.equals(category, "Science")) {
+      question = scienceQuestions.removeFirst();
+      System.out.println(question);
+    } else if (Objects.equals(category, "Sports")) {
+      question = sportsQuestions.removeFirst();
+      System.out.println(question);
+    } else if (Objects.equals(category, "Rock")) {
+      question = rockQuestions.removeFirst();
+      System.out.println(question);
+    }
+    return question;
   }
 }
