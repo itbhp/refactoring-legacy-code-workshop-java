3ebc62f8ba3bd6e9cfece4295ce570eea023bd30
Paolo Sciarra <itbhps@gmail.com>
2017-10-25 09:56:22 +0200

    added undeclared case but still admitted in the board case for last category -> Rock

--- TriviaRefactoringKata/src/test/java/com/workshop/refactoring/QuestionDeckTest.java
+++ TriviaRefactoringKata/src/test/java/com/workshop/refactoring/QuestionDeckTest.java
@@ -15,7 +15,8 @@ public class QuestionDeckTest {
   @Parameters({
                "0, Pop","4, Pop","8, Pop",
                "1, Science","5, Science","9, Science",
-               "2, Sports","6, Sports","10, Sports"
+               "2, Sports","6, Sports","10, Sports",
+               "3, Rock","7, Rock","11, Rock",
               })
   public void placeForCategory(Integer expectedPlace, String expectedCategory) throws Exception {
     final QuestionDeck deck = new QuestionDeck();
