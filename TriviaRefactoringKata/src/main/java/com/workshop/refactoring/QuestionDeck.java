06ef8790a9ec4c28021d1adf548b5e547381fb25
Paolo Sciarra <itbhps@gmail.com>
2017-10-25 11:03:18 +0200

    removed chained if with map

--- TriviaRefactoringKata/src/main/java/com/workshop/refactoring/QuestionDeck.java
+++ TriviaRefactoringKata/src/main/java/com/workshop/refactoring/QuestionDeck.java
@@ -1,61 +1,45 @@
 package com.workshop.refactoring;
 
-import java.util.LinkedList;
-import java.util.Objects;
+import java.util.*;
 
 public class QuestionDeck {
-  private LinkedList popQuestions;
-  private LinkedList scienceQuestions;
-  private LinkedList sportsQuestions;
-  private LinkedList rockQuestions;
+
+  private Map<String, LinkedList> questions = new HashMap<>();
 
   public QuestionDeck() {
-    popQuestions = new LinkedList();
-    scienceQuestions = new LinkedList();
-    sportsQuestions = new LinkedList();
-    rockQuestions = new LinkedList();
+    questions.put("Pop", new LinkedList());
+    questions.put("Science", new LinkedList());
+    questions.put("Sports", new LinkedList());
+    questions.put("Rock", new LinkedList());
   }
 
   void fillQuestions() {
-      for (int i = 0; i < 50; i++) {
-          popQuestions.addLast("Pop Question " + i);
-          scienceQuestions.addLast(("Science Question " + i));
-          sportsQuestions.addLast(("Sports Question " + i));
-          rockQuestions.addLast("Rock Question " + i);
+    for (int i = 0; i < 50; i++) {
+      for (String type : questions.keySet()) {
+        questions.get(type).addLast(type + " Question " + i);
       }
+    }
   }
 
   String currentCategoryForPlaces(int place) {
-      if (place == 0) return "Pop";
-      if (place == 4) return "Pop";
-      if (place == 8) return "Pop";
-      if (place == 1) return "Science";
-      if (place == 5) return "Science";
-      if (place == 9) return "Science";
-      if (place == 2) return "Sports";
-      if (place == 6) return "Sports";
-      if (place == 10) return "Sports";
-      return "Rock";
+    if (place == 0) return "Pop";
+    if (place == 4) return "Pop";
+    if (place == 8) return "Pop";
+    if (place == 1) return "Science";
+    if (place == 5) return "Science";
+    if (place == 9) return "Science";
+    if (place == 2) return "Sports";
+    if (place == 6) return "Sports";
+    if (place == 10) return "Sports";
+    return "Rock";
   }
 
   Object askQuestionForCategory(String category) {
-      Object question = null;
-      if (Objects.equals(category, "Pop")) {
-        question = popQuestions.removeFirst();
-        System.out.println(question);
-      }
-      else if (Objects.equals(category, "Science")) {
-        question = scienceQuestions.removeFirst();
-        System.out.println(question);
-      }
-      else if (Objects.equals(category, "Sports")) {
-        question = sportsQuestions.removeFirst();
-        System.out.println(question);
-      }
-      else if (Objects.equals(category, "Rock")) {
-        question = rockQuestions.removeFirst();
-        System.out.println(question);
-      }
-      return question;
+    Object question = null;
+    if (questions.containsKey(category)) {
+      question = questions.get(category).removeFirst();
+      System.out.println(question);
+    }
+    return question;
   }
 }
