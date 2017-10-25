3b26770ca812ead85a05c32d986d9dcd8bf96d85
Paolo Sciarra <itbhps@gmail.com>
2017-10-25 09:54:37 +0200

    added more cases for placeForCategory

--- TriviaRefactoringKata/src/test/java/com/workshop/refactoring/QuestionDeckTest.java
+++ TriviaRefactoringKata/src/test/java/com/workshop/refactoring/QuestionDeckTest.java
@@ -12,7 +12,11 @@ import static org.junit.Assert.*;
 public class QuestionDeckTest {
 
   @Test
-  @Parameters({"0, Pop","4, Pop","8, Pop", "1, Science","5, Science","9, Science"})
+  @Parameters({
+               "0, Pop","4, Pop","8, Pop",
+               "1, Science","5, Science","9, Science",
+               "2, Sports","6, Sports","10, Sports"
+              })
   public void placeForCategory(Integer expectedPlace, String expectedCategory) throws Exception {
     final QuestionDeck deck = new QuestionDeck();
     assertThat(deck.currentCategoryForPlaces(expectedPlace), is(expectedCategory));
